9f5e15bacec5b5238170e000e26fa446f76e099f
SebastiaanvanEtten <vanettensebastiaan@gmail.com>
2019-11-29 12:32:42 +0100

    filteren van buurten en wijken fix

--- 4.Opschonen van alle datasets.R
+++ 4.Opschonen van alle datasets.R
@@ -13,6 +13,20 @@ buurten2016 <- allsets[[3]]
 buurten2017 <- allsets[[4]]
 buurten2018 <- allsets[[5]]
 
+#filter alle wijken uit de dataset
+buurten2018 <- buurten2018[which(buurten2018$Soort_regio_omschrijving == "Buurt"), ]
+buurten2017 <- buurten2017[which(buurten2017$Soort_regio_omschrijving == "Buurt"), ]
+buurten2016 <- buurten2016[which(buurten2016$Soort_regio_omschrijving == "Buurt"), ]
+buurten2015 <- buurten2015[which(buurten2015$Soort_regio_omschrijving == "Buurt"), ]
+buurten2014 <- buurten2014[which(buurten2014$Soort_regio_omschrijving == "Buurt"), ]
+
+#rownames(buurten2018) <- make.unique(rownames(buurten2018))
+
+
+for (i in 1:nrow(buurten2014)) {
+  xy <- buurten2016[which(buurten2016$Codering_code == buurten2014$Codering_code[i]),]
+  buurten2014[i,"_Wijken_en_buurten"] <- xy$`_Wijken_en_buurten`
+}
 
 ## Alle kolommen namen uit buurten2014 wegschrijven naar k.naam 
 k.naam <- colnames(buurten2014)
@@ -38,14 +52,16 @@ g2018 <- MergeGemeenteJaar("Data/Gemeente/",2018)
 
 ## DIT IS TIJDELIJKE CODE OM DE DESCISION TREE WERKEND TE HOUDEN
 buurten2018 <- merge(x = buurten2018, y = g2018, by = "Codering_code")
-buurten2015 <- buurten2015[which(buurten2018$Codering_code %in% buurten2018$Codering_code), ]
-buurten2016 <- buurten2016[which(buurten2018$Codering_code %in% buurten2018$Codering_code), ]
-buurten2017 <- buurten2017[which(buurten2018$Codering_code %in% buurten2018$Codering_code), ]
-buurten2014 <- buurten2014[which(buurten2018$Codering_code %in% buurten2018$Codering_code), ]
+buurten2015 <- buurten2015[which(buurten2015$Codering_code %in% buurten2018$Codering_code), ]
+buurten2016 <- buurten2016[which(buurten2016$Codering_code %in% buurten2018$Codering_code), ]
+buurten2017 <- buurten2017[which(buurten2017$Codering_code %in% buurten2018$Codering_code), ]
+buurten2014 <- buurten2014[which(buurten2014$Codering_code %in% buurten2018$Codering_code), ]
 buurten2018 <- allsets[[5]]
-buurten2018 <- buurten2018[which(buurten2014$Codering_code %in% buurten2014$Codering_code), ]
-btotaal2 <- bind_rows(buurten2018, buurten2017, buurten2016, buurten2015, buurten2014)  
+buurten2018 <- buurten2018[which(buurten2018$Soort_regio_omschrijving == "Buurt"), ]
+buurten2018 <- buurten2018[which(buurten2018$Codering_code %in% buurten2016$Codering_code), ]
 ##
+
+btotaal2 <- bind_rows(buurten2018, buurten2017, buurten2016, buurten2015, buurten2014)
 
 
 buurten2014 <- allsets[[1]]
@@ -54,24 +70,6 @@ buurten2016 <- allsets[[3]]
 buurten2017 <- allsets[[4]]
 buurten2018 <- allsets[[5]]
 
-
-buurten2014 <- merge(x = buurten2014, y = g2014, by = "Codering_code")
-buurten2016 <- merge(x = buurten2016, y = g2016, by = "Codering_code")
-buurten2018 <- merge(x = buurten2018, y = g2018, by = "Codering_code")
-buurten2015 <- buurten2015[which(buurten2015$Codering_code %in% buurten2018$Codering_code), ]
-buurten2017 <- buurten2017[which(buurten2017$Codering_code %in% buurten2018$Codering_code), ]
-
-buurten2016$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`<- as.numeric(buurten2016$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`)
-buurten2018$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`<- as.numeric(buurten2018$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`)
-
-btotaal <- bind_rows(buurten2018, buurten2017, buurten2016, buurten2015, buurten2014)  
-rm(list = c("g2018", "g2016", "g2014"))
-
-
-
-#bind_rows(buurten2018, buurten2017, buurten2016, buurten2015, buurten2014) -> btotaal  
-
-poly.g <- geojsonio::geojson_read("Data/Polygons/Rotterdam Wijken_2.geojson", what = "sp")
 
 #filter alle wijken uit de dataset
 buurten2018 <- buurten2018[which(buurten2018$Soort_regio_omschrijving == "Buurt"), ]
@@ -85,6 +83,40 @@ for (i in 1:nrow(buurten2014)) {
   buurten2014[i,"_Wijken_en_buurten"] <- xy$`_Wijken_en_buurten`
 }
 
+## Alle kolommen namen uit buurten2014 wegschrijven naar k.naam 
+k.naam <- colnames(buurten2014)
+
+for(j in 1:108) {
+  if (j >= 9 & j <= 24) {
+    k.naam[j] <- gsub("\\_%", "_aantal", x = colnames(buurten2014[j]))
+  }
+  else{
+    k.naam[j] <- colnames(buurten2014[j])
+  }
+}
+
+colnames(buurten2014) <- k.naam
+
+
+buurten2014 <- merge(x = buurten2014, y = g2014, by = "Codering_code")
+buurten2016 <- merge(x = buurten2016, y = g2016, by = "Codering_code")
+buurten2018 <- merge(x = buurten2018, y = g2018, by = "Codering_code")
+buurten2015 <- buurten2015[which(buurten2015$Codering_code %in% buurten2018$Codering_code), ] 
+buurten2017 <- buurten2017[which(buurten2017$Codering_code %in% buurten2018$Codering_code), ] 
+
+buurten2016$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`<- as.numeric(buurten2016$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`)
+buurten2018$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`<- as.numeric(buurten2018$`Si_X..bewoners.dat.bekend.is.met.het.Steunpunt.Mantelzorg`)
+
+btotaal <- bind_rows(buurten2018, buurten2017, buurten2016, buurten2015, buurten2014)  
+rm(list = c("g2018", "g2016", "g2014"))
+
+
+
+#bind_rows(buurten2018, buurten2017, buurten2016, buurten2015, buurten2014) -> btotaal  
+
+poly.g <- geojsonio::geojson_read("Data/Polygons/Rotterdam Wijken_2.geojson", what = "sp")
+
+
 #merge de data van cbs2018 met de geo data van 2018
 # buurtcode en Codering_code zijn bijde kolommen met de code van wijk, hierop worden ze gemerged 
 Buurt.Pol2018 <- merge(x = poly.g, y = buurten2018, by.x = "buurtcode", by.y = "Codering_code", all.y = TRUE)
@@ -93,7 +125,6 @@ Buurt.Pol2016 <- merge(x = poly.g, y = buurten2016, by.x = "buurtcode", by.y = "
 Buurt.Pol2015 <- merge(x = poly.g, y = buurten2015, by.x = "buurtcode", by.y = "Codering_code", all.y = TRUE)
 Buurt.Pol2014 <- merge(x = poly.g, y = buurten2014, by.x = "buurtcode", by.y = "Codering_code", all.y = TRUE)
 
-rownames(buurten2018) <- make.unique(buurten2018[,2])
 
 #rm(list = c("buurten2018", "buurten2017", "buurten2016", "buurten2015", "buurten2014"))
 #rm(list = c("BuurtEnPoly2018", "BuurtEnPoly2017", "BuurtEnPoly2016", "BuurtEnPoly2015", "BuurtEnPoly2014"))
