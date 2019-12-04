fe6dcb48b1724e50c17d50d1d12c4c6f0f3e4b0f
Lennard Robert Kras <lennardrk@outlook.com>
2019-12-04 13:01:15 +0100

    Generalized the method, to make a neural model on the data, into a class

--- Examples/Neural network Mushrooms/neural.py
+++ Examples/Neural network Mushrooms/neural.py
@@ -16,7 +16,7 @@ from keras.models            import Sequential
 from keras.layers            import Dense
 
 datapath = "data/mushrooms.csv"
-shrooms = pd.read_csv(datapath)
+#shrooms = pd.read_csv(datapath)
 
 # Ik liep tegen problemen aan doordat op sommige momenten een kolom allen maar
 # numerieke waarde kon bestaan. Daarom wijs ik met behulp van deze class een 
@@ -53,52 +53,89 @@ class ClassifierTranslator:
     def TranslateListBackward(self, colname: str, l: list) -> list:
         return list(map(lambda x: self.TranslateValueBackward(colname, x), l))
 
-translator = ClassifierTranslator(shrooms)
+#translator = ClassifierTranslator(shrooms)
 
-#Voorbeelden...
-#print(translator.TranslateValueBackward("class", 1))
-#print(translator.TranslateListBackward("class", [0,1,2]))
-#print(translator.TranslateListForward("class", ["p","e","p"]))
+##Voorbeelden...
+##print(translator.TranslateValueBackward("class", 1))
+##print(translator.TranslateListBackward("class", [0,1,2]))
+##print(translator.TranslateListForward("class", ["p","e","p"]))
 
-# Modeleren van het model
-## Omzetten van alle 'categorische' waarden naar getallen.
-X = shrooms.drop("class", axis = 1)
-Y = DataFrame(translator.TranslateListForward("class",shrooms["class"]))
-for col in X.columns:
-    X[col] = translator.TranslateListForward(col, X[col])
+#NOTE: Als er 2 _ voor een attribuut naam staan in een python class
+# dan is het vaak ten zeerste aangeraden om niks met dit attribuut te doen.
+# Het word dan als private field/attribuut gezien worden net als in talen als
+# C++ , C#, Java.
 
+class NeuralNetwork:
+    
+    def __init__(self, 
+                 datasetPath: str, 
+                 epochs: int,
+                 batch_size: int,
+                 yColName: str):
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+        dataset = pd.read_csv(datasetPath)
+        self.translator = ClassifierTranslator(dataset)
+        
+        ### Omzetten van alle 'categorische' waarden naar getallen.
+        X = dataset.drop(yColName, axis = 1)
+        Y = DataFrame(self.translator.TranslateListForward(colname = yColName,
+                                                           l = dataset[yColName]))
+        for col in X.columns:
+            X[col] = self.translator.TranslateListForward(colname = col, 
+                                                          l = X[col])
+        
+        self.__X_train, \
+        self.__X_test, \
+        self.__Y_train, \
+        self.__Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+        
+        ##Hoeveelheid features in X (22)
+        classN = len(X.columns)
+        
+        ##initializeert het model
+        self.model = Sequential()
+        
+        ## Eerste 'hidden' laag met 22 features. In een voorbeeld zag ik 
+        ## dat de (hoeveelheid features * 8) de parameter units moest zijn, al kon ik
+        ## niet echt teruglezen waarom.
+        self.model.add(Dense(classN * 8, 
+                             input_dim = classN, 
+                             activation='relu'))
+        
+        ##Tweede hidden laag
+        self.model.add(Dense(classN * 4, 
+                             activation ='relu'))
+        
+        ##Laatste sigmoid laag
+        self.model.add(Dense(1, 
+                             activation='sigmoid'))
+        
+        self.model.compile(loss = 'binary_crossentropy',
+                           optimizer = 'adam',
+                           metrics = ['accuracy'])
 
-#initializeert het model
-model = Sequential() 
+        self.model.fit(self.__X_train,
+                       self.__Y_train,
+                       epochs=epochs, 
+                       batch_size=batch_size)
+        
+        _, self.accuracy = self.model.evaluate(self.__X_test, self.__Y_test)
+        print('Accuracy: %.2f' % (self.accuracy*100))
+        
+    def Predict(self, data: DataFrame):
+        return self.model.predict(data)
 
-#Hoeveelheid features in X (22)
-classN = len(X.columns) 
+network = NeuralNetwork(datasetPath = datapath, 
+                        epochs = 150, 
+                        batch_size = 10, 
+                        yColName = "class")
 
-# Eerste 'hidden' laag met 22 features. In een voorbeeld zag ik 
-# dat de (hoeveelheid features * 8) de parameter units moest zijn, al kon ik
-# niet echt teruglezen waarom.
-model.add(Dense(classN * 8, input_dim= classN, activation="relu")) 
+##Fitten van het model
+##Epochs: een trainings ronde
+##batch_size: hoeveel rows er tijdens 1 epoch iteratief worden verwerkt tot het
+## het einde van de data is bereikt.
 
-#Tweede hidden laag
-model.add(Dense(classN * 4, activation="relu")) 
-
-#Laatste sigmoid laag
-model.add(Dense(1, activation="sigmoid")) 
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
-
-#Fitten van het model
-#Epoch: een trainings ronde
-#batch_size: Een hoeveelheid rows die tijdens 1 epoch worden verwerkt. Dat wil zeggen: 
-#Dus: Hoe meer epochs, hoe meer je model traint. Hoe grote de batch size, hoe meer er tijdens 1 epoch behandeld word.
-model.fit(X_train,Y_train,epochs=150, batch_size=10)
-
-_, accuracy = model.evaluate(X_test, Y_test)
-print('Accuracy: %.2f' % (accuracy*100))
-
-predictions = model.predict(X_test)
 #TODO:
 # - Wat doet de parameter van Dense - units überhaupt?
 # - Correct bepalen van epochs en batch size
+
